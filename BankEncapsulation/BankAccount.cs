9dfe4de4cb1abbf6e767fc1a2ef77681bb7e27ea
Felix Lange <felixlange1@gmail.com>
2024-12-30 22:09:45 -0800

    added bank account class

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,16 @@
+namespace BankEncapsulation;
+
+public class BankAccount
+{
+    private double balance = 0;
+
+    public void Deposit(double depositAmount)
+    {
+        balance += depositAmount;
+    }
+
+    public double GetBalance()
+    {
+        return balance;
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount myAccount = new BankAccount();
+
+            Console.WriteLine("Press 1 for account balance or 2 to make a deposit");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                Console.WriteLine($"Your new account balance is {myAccount.GetBalance()}");
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Please enter deposit amount");
+                double depositAmount = double.Parse(Console.ReadLine());
+                myAccount.Deposit(depositAmount);
+                Console.WriteLine($"Your new account balance is {myAccount.GetBalance()}");
+            }
+            
         }
     }
 }
